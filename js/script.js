fbc56ffadf5daba502af9371aeb202ffca51a052
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2024-06-25 17:25:05 -0600

    refactor: rename section and associated classes from 'metodologia' to 'methodology'

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// Function menu
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const navList = document.querySelector('nav ul');
@@ -5,4 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
     hamburgerMenu.addEventListener('click', () => {
         navList.classList.toggle('open');
     });
+
+    // Function Button
+    const contactButton = document.getElementById('contact-button');
+    if (contactButton) {
+        contactButton.addEventListener('click', function(event) {
+            event.preventDefault(); // Previene el comportamiento predeterminado del enlace
+            document.getElementById('contacto').scrollIntoView({ behavior: 'smooth' });
+        });
+    }
 });
